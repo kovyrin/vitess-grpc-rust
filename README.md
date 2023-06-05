636fd223254a53a0b21c3718d0f773f94991f996
Oleksiy Kovyrin <oleksiy.kovyrin@shopify.com>
2023-06-05 12:44:58 -0400

    Add package info, readme, example

---
+++ README.md
@@ -0,0 +1,34 @@
+# gRPC Client for Vitess
+
+This is a Rust gRPC client for Vitess. It is generated from the Vitess proto files using
+[tonic](https://github.com/hyperium/tonic).
+
+## Usage
+
+To use this crate, add the following to your Cargo.toml:
+
+```toml
+[dependencies]
+vitess_grpc = "0.1"
+tokio = { version = "1.0", features = ["full"] }
+```
+
+## Example
+
+You can find an example of how to use this crate in the [examples](examples) directory.
+
+Examples can be run with:
+
+```shell
+cargo run --example <example_name>
+```
+
+Available examples are:
+* [vstream-consumer](examples/vstream.rs): A simple vstream consumer, which prints the events it receives.
+
+## License
+
+The files in the `proto` directory are copied from the [Vitess repository](https://github.com/vitessio/vitess/tree/main/proto) and are licensed under the Apache License, Version 2.0. See the
+[Vitess LICENSE](https://github.com/vitessio/vitess/blob/main/LICENSE) file for more details.
+
+The rest of the code in this repository is licensed under the MIT license. See the [LICENSE](LICENSE) file for more details.
--- examples/vstream.rs
+++ examples/vstream.rs
@@ -7,12 +7,14 @@ use vitess_grpc::vtgateservice::vitess_client::VitessClient;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = VitessClient::connect("http://localhost:16113").await.unwrap();
 
+    // Configure the details of VStream
     let vstream_flags = VStreamFlags {
         stop_on_reshard: true,
         heartbeat_interval: 5,
         ..Default::default()
     };
 
+    // Start from the current position
     let initial_position = VGtid {
         shard_gtids: vec![
             ShardGtid {
@@ -24,15 +26,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         ],
     };
 
+    // Make the VStream API request to start streaming changes from the cluster
     let request = VStreamRequest {
         vgtid: Some(initial_position),
         tablet_type: TabletType::Primary.into(),
         flags: Some(vstream_flags),
         ..Default::default()
     };
-
     let response = client.v_stream(request).await.unwrap();
 
+    // Stream messages one at a time
     let mut resp_stream = response.into_inner();
     while let Some(message) = resp_stream.message().await? {
         println!("Received {:?}", message);
